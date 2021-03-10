8793447ab96a718027ebb8a6f925abeaed8cf180
Amaru <amaruescalante@gmail.com>
2021-03-09 19:27:54 -0500

    added new logo

--- grupal_9/src/components/Sidebar.js
+++ grupal_9/src/components/Sidebar.js
@@ -5,7 +5,7 @@ export default function Sidebar() {
     return (
         <div>
             <div className="bg-light border-right" id="sidebar-wrapper">
-                <div className="sidebar-heading">Start Bootstrap </div>
+                <div className="logo sidebar-heading">Luteach</div>
                 <div className="list-group list-group-flush">
                     <Link to="/" className="list-group-item list-group-item-action bg-light">Inicio</Link>
                     <Link to="/stats" className="list-group-item list-group-item-action bg-light">Estadísticas</Link>
