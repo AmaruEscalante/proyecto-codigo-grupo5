4c79889880daa101ba5a6661f746abb539e92ff1
david <davidbgemin@gmail.com>
2021-03-11 17:24:50 -0500

    chart_luteachers

--- grupal_9/src/pages/Chat.js
+++ grupal_9/src/pages/Chat.js
@@ -12,11 +12,21 @@ export default function Chat() {
                 <div id="card" className="teacher">
                     <div id="card-body">
                         <div id="card-title">
-                            <img src={Profesor} class="card-img-top" alt="..."></img>
+                            <img src={Profesor} className="card-img-top" alt="..."></img>
                             <h5>Luis</h5>
                         </div>
 
-                        <p class="card-text">Buenos días, en qué puedo ayudarte?</p>
+                        <p className="card-text">Buenos días, en qué puedo ayudarte?</p>
+                    </div>
+                </div>
+
+                <div id="card" className="student">
+                    <div id="card-body">
+                        <div id="card-title">
+                            <img src={Alumno} className="card-img-top" alt="..."></img>
+                            <h5>User</h5>
+                        </div>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
                 </div>
 
@@ -26,23 +36,13 @@ export default function Chat() {
                             <img src={Alumno} class="card-img-top" alt="..."></img>
                             <h5>User</h5>
                         </div>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
                 </div>
 
-                <div id="card" className="student">
-                    <div id="card-body">
-                        <div id="card-title">
-                            <img src={Alumno} class="card-img-top" alt="..."></img>
-                            <h5>User</h5>
-                        </div>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                </div>
-
-            <form class="form-inline">
-                <input class="form-control mr-sm-2 form" id="message" type="search" placeholder="Escribe tu mensaje" aria-label="Search"></input>
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+            <form className="form-inline">
+                <input className="form-control mr-sm-2 form" id="message" type="search" placeholder="Escribe tu mensaje" aria-label="Search"></input>
+                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
                     <img src={Clip}></img>
                 </button>
             </form>
--- grupal_9/src/pages/Luteachers.js
+++ grupal_9/src/pages/Luteachers.js
@@ -1,9 +1,77 @@
 import React from 'react'
+import '../assets/luteachers.css'
+import { Bar, Line, Radar } from 'react-chartjs-2'
+
+
 
 export default function Luteachers() {
     return (
-        <div>
-            <h1>Hola</h1>
+        <div className="container_luteacher">
+            <div className="chart_luteachers">
+                <div class="card" style={{ minWidth: "30rem" }}>
+                    <div class="card-body grafico">
+                        <h5 class="card-title titulo">Lista de Luteachers</h5>
+
+                        {/* Inicio gráfica */}
+                        <div className="container-12">
+                            <div className="row barra">
+
+                                <Bar
+                                    data={{
+                                        labels: ['UTP', 'U.Lima', 'Pacífico', 'Cayetano H.', 'U. Católica', 'U. Piura',],
+                                        datasets: [{
+                                            label: 'Cantidad de inscritos',
+                                            data: [7, 12, 13, 9, 17, 3],
+                                            backgroundColor: [
+                                                'rgba(255, 99, 132, 0.2)',
+                                                'rgba(54, 162, 235, 0.2)',
+                                                'rgba(255, 206, 86, 0.2)',
+                                                'rgba(75, 192, 192, 0.2)',
+                                                'rgba(153, 102, 255, 0.2)',
+                                                'rgba(255, 159, 64, 0.2)'
+                                            ],
+                                            borderColor: [
+                                                'rgba(255, 99, 132, 1)',
+                                                'rgba(54, 162, 235, 1)',
+                                                'rgba(255, 206, 86, 1)',
+                                                'rgba(75, 192, 192, 1)',
+                                                'rgba(153, 102, 255, 1)',
+                                                'rgba(255, 159, 64, 1)'
+                                            ],
+                                            borderWidth: 1,
+                                        }],
+                                    }}
+                                    height={150}
+                                    width={200}
+                                    options={{
+                                        maintainAspectRatio: false,
+                                        scales: {
+                                            yAxes: [
+                                                {
+                                                    ticks: {
+                                                        beginAtZero: true
+                                                    }
+                                                }
+                                            ]
+                                        },
+                                    }}></Bar>
+                            </div>
+
+                        </div>
+
+
+                        {/* Fin gráfica */}
+
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Seleccionar </button>
+                            <div class="dropdown-menu">
+                                <a class="dropdown-item" href="#">Semana</a>
+                                <a class="dropdown-item" href="#">Mes</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
