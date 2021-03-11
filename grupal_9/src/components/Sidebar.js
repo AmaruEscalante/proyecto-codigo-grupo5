0cacadccb947da549282a8354397758dee6f29cd
david <davidbgemin@gmail.com>
2021-03-10 20:22:11 -0500

    ls_active

--- grupal_9/src/components/Sidebar.js
+++ grupal_9/src/components/Sidebar.js
@@ -7,11 +7,10 @@ export default function Sidebar() {
             <div className="bg-light border-right" id="sidebar-wrapper">
                 <div className="logo sidebar-heading">Luteach</div>
                 <div className="list-group list-group-flush">
-                    <Link to="/" className="list-group-item list-group-item-action bg-light">Inicio</Link>
-                    <Link to="/stats" className="list-group-item list-group-item-action bg-light">Estadísticas</Link>
-                    <a href="#" className="list-group-item list-group-item-action bg-light">Clientes</a>
-                    <a href="#" className="list-group-item list-group-item-action bg-light">Luteachers</a>
-                    <a href="#" className="list-group-item list-group-item-action bg-light">Postulantes</a>
+                    <Link to="/" className="list-group-item list-group-item-action bg-light" data-toggle="list">Inicio</Link>
+                    <Link to="/stats" className="list-group-item list-group-item-action bg-light" data-toggle="list">Estadísticas</Link>
+                    <a href="#" className="list-group-item list-group-item-action bg-light" data-toggle="list">Clientes</a>
+                    <a href="#" className="list-group-item list-group-item-action bg-light" data-toggle="list">Luteachers</a>
                 </div>
             </div>
         </div>
