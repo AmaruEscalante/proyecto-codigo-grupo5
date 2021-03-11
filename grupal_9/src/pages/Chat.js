1c310516f78b5f88e8b9be8f9349ca8d91a9ffcb
david <davidbgemin@gmail.com>
2021-03-11 01:53:53 -0500

    chat_mejoras_pendientes

--- grupal_9/src/pages/Chat.js
+++ grupal_9/src/pages/Chat.js
@@ -1,36 +1,33 @@
 import React from 'react'
 import '../assets/chat.css'
+import Alumno from '../assets/img/alumno.png'
+import Profesor from '../assets/img/profesor.svg'
 
 export default function Chat() {
     return (
         <div className="chat">
+            <div className="container">
+                <div className="box alumno">
+                    <img src={Alumno} className="imagen"></img>
+                    <p>Hello. How are you today?</p>
+                </div>
 
-<div class="container">
-  <img src="/w3images/bandmember.jpg" alt="Avatar"></img>
-  <p>Hello. How are you today?</p>
-  <span class="time-right">11:00</span>
-</div>
+                <div className="box profesor">
+                    <img src={Profesor} className="imagen"></img>
+                    <p>Hey! I'm fine. Thanks for asking!</p>
+                </div>
 
-<div class="container darker">
-  <img src="/w3images/avatar_g2.jpg" alt="Avatar" class="right"></img>
-  <p>Hey! I'm fine. Thanks for asking!</p>
-  <span class="time-left">11:01</span>
-</div>
+                <div className="box alumno">
+                    <img src={Alumno} className="imagen"></img>
+                    <p>Sweet! So, what do you wanna do today?</p>
+                </div>
 
-<div class="container">
-  <img src="/w3images/bandmember.jpg" alt="Avatar"></img>
-  <p>Sweet! So, what do you wanna do today?</p>
-  <span class="time-right">11:02</span>
-</div>
+                <div className="box profesor">
+                    <img src={Profesor} className="imagen"></img>
 
-<div class="container darker">
-  <img src="/w3images/avatar_g2.jpg" alt="Avatar" class="right"></img>
-  <p>Nah, I dunno. Play soccer.. or learn more coding perhaps?</p>
-  <span class="time-left">11:05</span>
-</div>
-
-
-
+                    <p>Nah, I dunno. Play soccer.. or learn more coding perhaps?</p>
+                </div>
+            </div>
         </div>
     )
 }
